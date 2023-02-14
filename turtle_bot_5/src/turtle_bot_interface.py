87156310c6d7a2f9d256d86847ce55a3763071df
Julián Andrés Méndez <j.mendezm@uniandes.edu.co>
2023-02-14 06:25:57 -0500

    Punto2

--- turtle_bot_5/src/turtle_bot_interface.py
+++ turtle_bot_5/src/turtle_bot_interface.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 from threading import Thread
-
+from matplotlib.ticker import AutoMinorLocator
 
 fig, ax = plt.subplots(facecolor="#e5e5e5")
 x = np.arange(0, 4*np.pi, 0.01)
@@ -43,9 +43,10 @@ class MinimalSubscriber(Node, Thread):
 
 class VentanaTurtleBot(Thread):
 
-    def __init__(self, root):
+    def __init__(self, root, minimal_subscriber):
         # Configuración inicial root
         self.root = root
+        self.minimal_subscriber = minimal_subscriber
         self.root.geometry("800x800")
         self.root.title("Interfaz TurtleBot")
         self.root.minsize(width=800, height=800)
@@ -56,13 +57,13 @@ class VentanaTurtleBot(Thread):
         frame_top.pack(fill='both')
         
         frame_buttton = Frame(self.root, bg='white', bd=3)
-        frame_buttton.pack(expand=1, fill='both')
+        frame_buttton.pack(expand=1, fill='both')   
 
         # Top   
         Label(frame_top, text="Nombre de la gráfica:",bg="white", font='Helvetica 12 bold').pack(pady=5, side='left', expand=1)
         titulo = tkinter.StringVar(self.root, value='')
         Entry(frame_top, textvariable=titulo, width=40).pack(pady=5, side='left', expand=1)
-        Button(frame_top, text='Empezar', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=lambda: self.init(titulo.get())).pack(pady=5, side='left', expand=1)
+        Button(frame_top, text='Empezar', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=lambda: self.init(titulo)).pack(pady=5, side='left', expand=1)
 
         # Center
         global canvas 
@@ -72,8 +73,8 @@ class VentanaTurtleBot(Thread):
         # Bottom
         Button(frame_buttton, text='Detener', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=self.detener).pack(pady=5, side='left', expand=1)
         Button(frame_buttton, text='Reanudar', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=self.reanudar).pack(pady=5, side='left', expand=1)
-        Label(frame_buttton, text="Grupo 5 - Robótica IELE 3338", bg="white", font='Helvetica 12 bold').pack(pady=5, side='left', expand=1)
         Button(frame_buttton, text='Guardar', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=self.guardar).pack(pady=5, side='left', expand=1)
+        Button(frame_buttton, text='Salir', width=15, bg='white', fg='black', font='Helvetica 12 bold', command=self.salir).pack(pady=5, side='left', expand=1)
 
 
         self.root.mainloop()
@@ -84,25 +85,44 @@ class VentanaTurtleBot(Thread):
         return line,
 
     def init(self, titulo):
-        print(titulo)
         global ani 
-        plt.title(titulo, color='black', size=16)
+        plt.title(titulo.get(), color='black', size=16)
+        plt.xlabel("Posición x [m]", fontsize=12)
+        plt.ylabel("Posición y [m]", fontsize=12)
+        # Give plot a balck background like ggplot.
+        ax.set_facecolor('#000000')
+        # Remove border around plot.
+        [ax.spines[side].set_visible(False) for side in ax.spines]
+        # Style the grid.
+        ax.grid(which='major', color='white', linewidth=1.2)
+        ax.grid(which='minor', color='white', linewidth=0.6)
+        # Show the minor ticks and grid.
+        ax.minorticks_on()
+        # Now hide the minor ticks (but leave the gridlines).
+        ax.tick_params(which='minor', bottom=False, left=False)
+        # Only show minor gridlines once in between major gridlines.
+        ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+        ax.yaxis.set_minor_locator(AutoMinorLocator(2))
         ax.set_xlim(-2.3, 2.3)
         ax.set_ylim(-2.3, 2.3)
         ani = animation.FuncAnimation(fig, self.animate, interval=20, blit=True, save_count=10)
         canvas.draw()
-
 
     def guardar(self):
         for item in canvas.get_tk_widget().find_all():
             canvas.get_tk_widget().delete(item)
         plt.plot(xdata, ydata, color='m', linewidth=3, markersize=1, markeredgecolor='m')
         plt.show()
-        
+        self.salir()
+
+    def salir(self):
+        # Destroy all
+        self.root.destroy()
+        self.minimal_subscriber.destroy_node()
+        rclpy.shutdown()
 
     def detener(self):
         ani.event_source.stop()
-        
 
     def reanudar(self):
         ani.event_source.start()
@@ -117,13 +137,11 @@ def main(args=None):
     minimal_subscriber.start()
 
     root = Tk()
-    thread_plot = VentanaTurtleBot(root)
+    thread_plot = VentanaTurtleBot(root, minimal_subscriber)
 
-    # Destroy the node explicitly
+    
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
