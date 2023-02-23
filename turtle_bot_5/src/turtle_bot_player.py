5daf8ce6d756bd9e7b9ada6bdf169e26851b0d76
mmanuel11 <91902776+mmanuel11@users.noreply.github.com>
2023-02-23 17:25:25 -0500

    Update turtle_bot_player.py
    Funciona la lectura , y ademas se logra mover bot con txt

--- turtle_bot_5/src/turtle_bot_player.py
+++ turtle_bot_5/src/turtle_bot_player.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist 
 
 #librería para el cliente
 import sys
@@ -12,77 +13,97 @@ from turtle_bot_5.srv import SavePath
 import numpy as np
 
 mylines = []
-linearVel = 0
-angularVel = 0
+
 
 class MinimalPublisher(Node):
     def __init__(self):
-    	super().__init__('turtle_bot_player')
-    	self.publisher_ = self.create_publisher(Twist, 'turtlebot_cmdVel', self.publisher_callback,10)  	
+        super().__init__('turtle_bot_player')
+        self.publisher_ = self.create_publisher(Twist, 'turtlebot_cmdVel' ,10)    
+        #self.cli = self.create_client(SavePath, 'save_path')
+        #while not self.cli.wait_for_service(timeout_sec=1.0):
+        #    self.get_logger().info('service not available, waiting again...')	
+        #self.req = SavePath.Request()
+        #self.future = self.cli.call_async(self.req)
+        #rclpy.spin_until_future_complete(self, self.future)
+        #response = self.future.result()
+        #self.path = response.path
+	#CMabiar el path
+        archivo = "/home/robotica/taller1/path.txt"
+        self.mylines = self.converterTXT(archivo)
+        (self.linearVel,self.angularVel) = self.lineal_angular(archivo)
+        timer_period = 0.5
+        self.i = 0
+        self.timer = self.create_timer(timer_period, self.publisher_callback)
+        #self.timer = self.publisher_callback()
 
     def publisher_callback(self):
         msg = Twist()
-	for i in mylines:
-            if i==1:
-                msg.linear.x = linearVel
-            elif i==2:
-                msg.linear.x = -linearVel
-            elif i==3:
-                msg.angular.z  = -angularVel
-            elif i==4:
-                msg.angular.z = angularVel
+        if  self.i < len(self.mylines):
+            j = self.mylines[self.i]
+            if j==1:
+                msg.linear.x = self.linearVel
+            elif j==2:
+                msg.linear.x = -self.linearVel
+            elif j==3:
+                msg.angular.z  = -self.angularVel
+            elif j==4:
+                msg.angular.z = self.angularVel
             self.publisher_.publish(msg)
+            self.i +=1
+        else:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+            self.publisher_.publish(msg)
+
+
+    def converterTXT(self,archivo):
+        mylines = []
+
+        with open(archivo,'rt') as myfile:
+            for i in myfile:
+                i = i.rstrip() # Quitar espacio en caso de
+                num = ''
+                for x in i:
+                    if x.find(','): # COnvertir coma en stirng para ser separado en el arreglo
+                        num += str(x)
+
+                    else:   
+                        mylines.append(int(num))
+                        num = ""
+
+        return mylines
+# Quitar self en caso de que se coloque la función por fuera de la clase
+    def lineal_angular(self,archivo):
+        alerta=1
+        cont = 0
+        global velAngular
+        global velLineal
+        with open(archivo, 'rt') as myfile:
+            for y in myfile:
+                if y.find(',') and alerta == 1:
+                    cont += 1
+                    if cont == 1:
+                        velLineal = float(y)
+                    if cont == 2:
+                        velAngular = float(y)
+                        alerta = 0
+        return velLineal,velAngular
             
-class MinimalClientAsync():
-    self.cli = self.create_client(SavePath, 'save_path')
+class MinimalClientAsync(Node):
     def __init__(self):
-	super().__init__('turtle_bot_player')
-	self.cli = self.create_client(SavePath, 'save_path')
-	while not self.cli.wait_for_service(timeout_sec=1.0):
-	    self.get_logger().info('service not available, waiting again...')	
-	self.req = SavePath.Request()
+        super().__init__('turtle_bot_player')
+        self.cli = self.create_client(SavePath, 'save_path')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')	
+        self.req = SavePath.Request()
 	
     def send_request(self):
-	self.future = self.cli.call_async(self.req)
-	rclpy.spin_until_future_complete(self, self.future)
-	return self.future.result()
-
-
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
 
 def main(args=None):
     rclpy.init()
-    
-    # Ruta
-    minimal_client = MinimalClientAsync()
-    response = minimal_client.send_request()
-    minimal_client.get_logger().info('Path: %d' %(response.path))
-    archivo = response.path
-    minimal_client.destroy_node()
-    
-
-    #Leer archivo 
-    alerta=1
-    cont = 0
-       
-    with open(archivo,'rt') as myfile:
-        for y in myfile:
-            if y.find(',') and alerta == 1:
-                cont += 1
-                if cont == 1:
-                    linearVel = float(y)
-                if cont == 2:
-                    angularVel = float(y)
-                    alerta = 0
-        for i in myfile:
-            i = i.rstrip() # Quitar espacio en caso de
-            num = ''
-            for x in i:
-                if x.find(','): # COnvertir coma en stirng para ser separado en el arreglo
-                    num += str(x)
-
-                else:
-                    mylines.append(int(num))
-                    num = ""
     
     #publicar   
     minimal_publisher = MinimalPublisher()
